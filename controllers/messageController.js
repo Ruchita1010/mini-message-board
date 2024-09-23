5cf59f61ab71b217e0f81636346fb480d5c5b9b2
Ruchita Gosavi <ruchigosavi10@gmail.com>
2024-09-24 00:50:57 +0530

    Add validation for form inputs

--- controllers/messageController.js
+++ controllers/messageController.js
@@ -1,3 +1,5 @@
+import { body, validationResult } from 'express-validator';
+
 // to be replaced by a DB
 const messages = [
   {
@@ -16,7 +18,7 @@ export const getAllMessages = (req, res) => {
   res.render('index', {
     title: 'Mini Message Board',
     link: { href: '/new', text: 'message' },
-    messages: messages,
+    messages,
   });
 };
 
@@ -24,15 +26,48 @@ export const getMessageForm = (req, res) => {
   res.render('messageForm', {
     title: 'Message Form',
     link: { href: '/', text: 'home' },
+    errors: {},
+    formData: req.body,
   });
 };
 
-export const createNewMessage = (req, res) => {
-  const { message, username } = req.body;
-  messages.push({
-    message,
-    username,
-    created_on: new Date().toLocaleDateString(),
-  });
-  res.redirect('/');
-};
+const validateUser = [
+  body('message')
+    .trim()
+    .notEmpty()
+    .withMessage('Message is required')
+    .isLength({ min: 2, max: 500 })
+    .withMessage('Message must be between 5 to 500 characters'),
+  body('username')
+    .trim()
+    .notEmpty()
+    .withMessage('Username is required')
+    .isLength({ min: 2, max: 30 })
+    .withMessage('Username must be between 3 to 30 characters')
+    .matches(/^[\w.]+$/)
+    .withMessage('Username can only contain letters, numbers, _ or .'),
+];
+
+export const createNewMessage = [
+  validateUser,
+  (req, res) => {
+    const errors = validationResult(req).mapped();
+    if (JSON.stringify(errors) !== '{}') {
+      console.log(errors);
+      res.status(400).render('messageForm', {
+        title: 'Message Form',
+        link: { href: '/', text: 'home' },
+        errors: errors,
+        formData: req.body,
+      });
+      return;
+    }
+    const { message, username } = req.body;
+    messages.push({
+      message,
+      username,
+      created_on: new Date().toLocaleDateString(),
+    });
+    res.redirect('/');
+  },
+];
