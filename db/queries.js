78cc24819d2eab576c16851aad95312fc6002fe9
Ruchita Gosavi <ruchigosavi10@gmail.com>
2024-09-25 22:57:39 +0530

    Display most recent messages first

--- db/queries.js
+++ db/queries.js
@@ -2,7 +2,7 @@ import { pool } from './pool.js';
 
 export const retrieveAllMessages = async () => {
   const { rows } = await pool.query(
-    'SELECT username, message, created_on FROM users;'
+    'SELECT username, message, created_on FROM users ORDER BY created_on DESC;'
   );
   return rows;
 };
